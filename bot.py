f37976b3560346f7b6a923b3c76a1bc7279c75aa
askoltsmitov <sibirskiywolf@mail.ru>
2018-11-28 14:17:22 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,10 +10,28 @@ import os
 from async_timeout import timeout
 from functools import partial
 from youtube_dl import YoutubeDL
+from discord import opus
+OPUS_LIBS = ['libopus-0.x86.dll', 'libopus-0.x64.dll',
+             'libopus-0.dll', 'libopus.so.0', 'libopus.0.dylib']
 
 bot = commands.Bot(command_prefix='-')
 send_Resume = ""
 sent = ""
+
+def load_opus_lib(opus_libs=OPUS_LIBS):
+    if opus.is_loaded():
+        return True
+
+    for opus_lib in opus_libs:
+            try:
+                opus.load_opus(opus_lib)
+                return
+            except OSError:
+                pass
+
+    raise RuntimeError('Could not load an opus lib. Tried %s' %
+                       (', '.join(opus_libs)))
+load_opus_lib()
 
 ytdlopts = {
 	'format': 'bestaudio/best',
