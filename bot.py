a6e41ac82b4425e54be3aeddce2685c7ad86cf80
askoltsmitov <sibirskiywolf@mail.ru>
2018-12-07 21:15:18 +0800

    Test duration

--- bot.py
+++ bot.py
@@ -169,13 +169,13 @@ class MusicPlayer:
 			source.volume = self.volume
 			self.current = source
 
-			start = time.monotonic()
+			start = int(time.monotonic())
 			durTotal = divmod(source.duration, 60)
 			self._guild.voice_client.play(source, after=lambda _: self.bot.loop.call_soon_threadsafe(self.next.set))
 			self.np = await self._channel.send(f'**Сейчас играет: ** `{source.title}` by **{source.requester}**')
 			msg = await self._channel.send(str(0) + ":" + str(0) + "/" + str(durTotal[0]) + ":" + str(durTotal[1]))
 			while self.vc.is_playing():
-				durCurrent = divmod(int(time.monotonic() - start, 60))
+				durCurrent = divmod(int(time.monotonic()) - start, 60)
 				await msg.edit(content = str(durCurrent[0]) + ":" + str(durCurrent[1]) + "/" + str(durTotal[0]) + ":" + str(durTotal[1]))
 				time.sleep(0.5)
 			await self.next.wait()
