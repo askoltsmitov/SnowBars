bdc8762e5d5fcc83e76e0c2813dfac6d97e5a998
askoltsmitov <sibirskiywolf@mail.ru>
2020-05-16 00:51:11 +0800

    Add command Shelby

--- __init__.py
+++ __init__.py
@@ -290,7 +290,7 @@ class Music(commands.Cog):
 			await ctx.message.delete()
 			await ctx.trigger_typing()
 
-			print(os.getcwd)
+			print(os.getcwd())
 			#onlyfiles = [f for f in os.listdir(curDirect) if os.path.isfile(join(curDirect, f))]
 			#print(onlyfiles)
 
